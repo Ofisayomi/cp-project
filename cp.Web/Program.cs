bcebca04fd6d70cf39a91ed260dddf5a8e00ebeb
ofisayomi-talos <>
2024-05-17 00:06:18 +0100

    injected cosmos client to factory

--- cp.Web/Program.cs
+++ cp.Web/Program.cs
@@ -1,9 +1,12 @@
+using cp.Web.Persistence.DbClient;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<CosmosClientFactory>();
 
 var app = builder.Build();
 
