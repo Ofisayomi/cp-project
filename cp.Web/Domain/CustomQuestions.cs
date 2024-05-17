508c6d987c67bb6b675faf7cccb47fc5c6ce83f7
ofisayomi-talos <>
2024-05-17 14:05:58 +0100

    reverts changes

--- cp.Web/Domain/CustomQuestions.cs
+++ cp.Web/Domain/CustomQuestions.cs
@@ -1,13 +1,10 @@
-using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 
 namespace cp.Web.Domain;
 
 public class CustomQuestions : BaseEntity
 {
-    [Required(AllowEmptyStrings = false)]
     public string QuestionType { get; set; }
-    [Required(AllowEmptyStrings = false)]
     public string Question { get; set; }
     [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
     public List<string>? QuestionValues { get; set; }
